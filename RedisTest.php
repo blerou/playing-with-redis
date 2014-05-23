5213092983e9f5a4da9feedbcee2f036e820a135
blerou <sulik.szabolcs@gmail.com>
2014-05-23 22:50:01 +0200

    set

---
+++ RedisTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class RedisTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $errno = $errstr = null;
+        $sock = fsockopen("127.0.0.1", 6379, $errno, $errstr, 10);
+        $this->assertThat($sock, $this->isType("resource"));
+
+        fwrite($sock, "*3\r\n");
+        fwrite($sock, "$3\r\n");
+        fwrite($sock, "set\r\n");
+        fwrite($sock, "$3\r\n");
+        fwrite($sock, "foo\r\n");
+        fwrite($sock, "$3\r\n");
+        fwrite($sock, "bar\r\n");
+        $res = fread($sock, 1000);
+        $this->assertEquals("+OK\r\n", $res);
+    }
+}
