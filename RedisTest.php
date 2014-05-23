f91973f94e56986408e2e13b9612f779859f607b
blerou <sulik.szabolcs@gmail.com>
2014-05-23 22:47:47 +0200

    read back

--- RedisTest.php
+++ RedisTest.php
@@ -17,5 +17,13 @@ class RedisTest extends PHPUnit_Framework_TestCase
         fwrite($sock, "bar\r\n");
         $res = fread($sock, 1000);
         $this->assertEquals("+OK\r\n", $res);
+
+        fwrite($sock, "*2\r\n");
+        fwrite($sock, "$3\r\n");
+        fwrite($sock, "get\r\n");
+        fwrite($sock, "$3\r\n");
+        fwrite($sock, "foo\r\n");
+        $res = fread($sock, 1000);
+        $this->assertEquals("$3\r\nbar\r\n", $res, "$res something different");
     }
 }
